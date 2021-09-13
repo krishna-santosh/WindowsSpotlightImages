8ec7a5ea2479dc51678f5c49321e5b45c03ef513
KRISHNA SANTOSH <75202541+krishnasantosh11@users.noreply.github.com>
2021-09-13 20:50:56 +0530

    Update spotlight.py

--- spotlight.py
+++ spotlight.py
@@ -1,56 +1,74 @@
 #https://github.com/krishnasantosh11/WindowsSpotlightImages
 
-import os
-from pathlib import Path
-import shutil
+"""spotlight.py: This script helps you to get the windows spotlight wallpapers"""
 
-#default user path and drive letter
-userPath = os.path.expanduser('~')
-driveletter = userPath[:2]
+import os
+import shutil
+from pathlib import Path
+from PIL import Image
+
+__author__ = 'Krishna Santosh'
+
+user_path = os.path.expanduser('~')
+drive_letter = user_path[:2]
 
 #this is the source path of windows spotlight images 
-dir = f'{userPath}/AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets/'
+dir = f'{user_path}/AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets/'
+despath = f'{drive_letter}/Windows_Spotlight_Images'
+portrait = f'{despath}/Portrait'
+landscape = f'{despath}/Landscape'
+Horizontal = (1920, 1080)
+Vertical = (1080, 1920)
 
-#creating a new folder, or if the folder already exists using the folder's path as destination 
-despath = f'{driveletter}/Windows_Spotlight_Images'
-if os.path.exists(despath):
-    des = despath
-    print(f"Folder exists at ({des})")
-else:
+if os.path.exists(despath) == False:
     os.mkdir(despath)
-    des = despath
-    print(f'Folder Created at ({des})')
 
-#creating a new list to store the files in our windows spotlight folder 
+if os.path.exists(portrait) == False:
+    os.mkdir(portrait)
+
+if os.path.exists(landscape) == False:
+    os.mkdir(landscape)
+
 list = os.listdir(dir)   
         
-#creating an empty list to append our desired images
 assets = []
 
-#appending images that are above the size of 140kb
 for files in list:
     location = os.path.join(dir, files)
     size = os.path.getsize(location)
     if size > 143360:
         assets.append(files)
 
-#copying the files from assets to our destination folder
 for f in assets:
-    if os.path.exists(des+'/'+f+'.jpg') == False:
-        shutil.copy(src=dir+f, dst=des)
+    if os.path.exists(despath+'/'+f+'.jpg') == False and os.path.exists(portrait+'/'+f+'.jpg') == False and os.path.exists(landscape+'/'+f+'.jpg') == False:
+        shutil.copy(src=dir+f, dst=despath)
 
-#this function renames the files in the destination, i.e converting file type to .jpg
 def rename(img):
-    p = Path(des+'/'+img)
+    p = Path(despath+'/'+img)
     p.rename(p.with_suffix('.jpg'))
 
-#using a for loop and calling the funciton for each individual file
-for i in range(0,len(assets)):
-    if os.path.exists(des+'/'+assets[i]+'.jpg') == False:
-        rename(assets[i])
-        print(assets[i])
+for f in assets:
+    if os.path.exists(despath+'/'+f+'.jpg') == False  and os.path.exists(portrait+'/'+f+'.jpg') == False and os.path.exists(landscape+'/'+f+'.jpg') == False:
+        rename(f)
+        print(f)
 
-#displaying the folder
-os.system(f'start {os.path.realpath(des)}')
+def orientation(pic):
+    image = Image.open(pic)
+    if image.size == Horizontal:
+        return 'landscape'
+    elif image.size == Vertical:
+        return 'portrait'
+
+list_2 = os.listdir(despath)
+
+for file in list_2:
+    if '.jpg' in file:
+        if os.path.exists(portrait+'/'+file) == False and os.path.exists(landscape+'/'+file) == False:
+            if orientation(despath+'/'+file) == 'landscape':
+                shutil.move(despath+'/'+file, landscape)
+            elif orientation(despath+'/'+file) == 'portrait':
+                shutil.move(despath+'/'+file, portrait)
+
+os.system(f'start {os.path.realpath(despath)}')
 
 print('Done!')
